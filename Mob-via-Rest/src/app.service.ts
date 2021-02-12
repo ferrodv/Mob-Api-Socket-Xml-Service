55d687e9804763f161ee4779f06ec7d93f908b3a
Broncebat <ferdy_rod@hotmail.com>
2021-02-12 01:16:55 -0400

    [Add] Times Logs

--- Mob-via-Rest/src/app.service.ts
+++ Mob-via-Rest/src/app.service.ts
@@ -7,16 +7,25 @@ import { constants } from 'buffer';
 export class AppService {
   
   repositorio : Objeto[] = [];
+  log = new Date();
+
+  getTime(): string{
+    return (this.log.getFullYear() + "/" + this.log.getMonth() + "/" + this.log.getDate() + " " + this.log.getHours() + ":" + this.log.getMinutes() + ":" + this.log.getSeconds());
+  }
 
   wellcome(): string[] {
+
+    console.log("[" + this.getTime() + "] Wellcome request");
     return JSON.parse('{ "get_1" : "/api/consultar/data", "get_2" : "/api/consultar/estructura", "post" : "/api/crear", "delete" : "/api/eliminar/:nombre", "put_1" : "/api/accion/replicar", "put_2" : "/api/accion/restaurar"  }');
   }
 
   getData(): Objeto[] {
+    console.log("[" + this.getTime() + "] Consulta Data request");
     return this.repositorio;
   }
   
   getStruct(): string {
+    console.log("[" + this.getTime() + "] Consulta Estructura request");
     return  JSON.parse('{ "atributo_1" :"nombre", "atributo_2" : "fecha", "atributo_3" : "accion" }');
   }
 
@@ -26,12 +35,14 @@ export class AppService {
   }
 
   create(objDto : ObjetoDto): Objeto {
+    console.log("[" + this.getTime() + "] Create request");
     const obj: ObjetoDto = new ObjetoDto(objDto);
     this.repositorio.push(obj.toObjeto());
     return obj
   }
 
   delete(nombre : String): Objeto {
+    console.log("[" + this.getTime() + "] Delete request");
     const index = this.getObjetoIndex(nombre);
     const obj:Objeto = this.repositorio[Number(index)];
     if (index >= 0)
@@ -40,6 +51,7 @@ export class AppService {
   }
 
   replicarObjetos(objDto : ObjetoDto): String{
+    console.log("[" + this.getTime() + "] Replicar request");
     var msj: String;
     if (objDto.accion == "COMMIT")
       msj = "replicar_commit"
@@ -52,6 +64,7 @@ export class AppService {
   }
 
   restaurarObetos(objDto : ObjetoDto){
+    console.log("[" + this.getTime() + "] Restaurar request");
     var msj: String;
     if (objDto.accion == "COMMIT")
       msj = "restaurar_commit"
